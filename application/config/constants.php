fc05954405a007c069441493690f75a50c94472a
Sushanta Patra <109073039+sushanta-dtpl034@users.noreply.github.com>
2024-01-11 13:17:55 +0530

    constant variable changes for live file upload

--- application/config/constants.php
+++ application/config/constants.php
@@ -93,8 +93,8 @@ define('tbl_asset_subcat_file','AssetSubcatFileMst');
 
 //qrcode url
 // define('QRCODE_URL','http://localhost/travel_luggage/qr/');
-define('QRCODE_URL','https://sandbox.dahlia.tech/travel_luggage/qr/');
-// define('QRCODE_URL','https://www.my-bagtags.com/qr/');
+// define('QRCODE_URL','https://sandbox.dahlia.tech/travel_luggage/qr/');
+define('QRCODE_URL','https://www.my-bagtags.com/qr/');
 
 
 // Define an Ttitle Prefix constant
